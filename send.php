a1467b9bb5f426a8d82a39c763e75e7d30cf97cd
Sharif Ahmed <73880659+Sharif320@users.noreply.github.com>
2023-03-06 15:51:07 +0600

    Add files via upload

---
+++ send.php
@@ -0,0 +1,22 @@
+﻿<?php
+$errors  = '';
+$myemail = 'info@therealagentsofscottsdale.com';
+if (empty($_POST['email'])) {
+    $errors .= "\n Error: Required Field";
+}
+
+$email   = $_POST['email'];
+
+if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)) {
+    $errors .= "\n Error: Invalid Email Address";
+}
+
+if (empty($errors)) {
+    $to            = $myemail;
+    $email_subject = "Newsletter Subscription";
+    $email_body    = "Newsletter Enquiry. Details are given below. \n Email: $email";
+    $headers       = "From: $email";
+    
+    mail($to, $email_subject, $email_body, $headers);
+}
+?>
